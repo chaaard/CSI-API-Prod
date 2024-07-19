696088e227b104f619bb7f3bf9e2d8976b203484
Aethenwulf <fjuat.snrshopping@gmail.com>
2024-07-19 14:42:31 +0800

    2024/07/19 - Maintenance/Merhant
    2024/07/19 - Maintenance/Merhant
    Status is now a toggle button (Active/Inactive)

--- CSI.Application/Services/MerchantService.cs
+++ CSI.Application/Services/MerchantService.cs
@@ -18,7 +18,9 @@ namespace CSI.Application.Services
         {
             var getMerchant = new List<Merchant>();
             getMerchant = await _dbContext.Merchant
-                .FromSqlRaw($"SELECT DISTINCT A.CACSAN[MerchantCode] , A.CUNMFL[MerchantName], CAST(A.CUCSN AS VARCHAR(500)) + ' P'[MerchantNo] FROM OPENQUERY([SNR], 'SELECT A.CUCSN, A.CUNMFL, B.CACSAN FROM MMJDALIB.CIMCUS A INNER JOIN MMJDALIB.CRMCSA B ON A.CUCSN = B.CACSN') AS A WHERE NOT EXISTS (SELECT * FROM tbl_customer AS B WHERE B.CustomerCode = A.CACSAN OR B.CustomerName = A.CUNMFL OR TRY_CONVERT(decimal, B.CustomerCode) = A.CUCSN);")
+                .FromSqlRaw($"SELECT DISTINCT A.CACSAN[MerchantCode] , A.CUNMFL[MerchantName], CAST(A.CUCSN AS VARCHAR(500)) + ' P'[MerchantNo] " +
+                            $"FROM OPENQUERY([SNR], 'SELECT A.CUCSN, A.CUNMFL, B.CACSAN FROM MMJDALIB.CIMCUS A INNER JOIN MMJDALIB.CRMCSA B ON A.CUCSN = B.CACSN') AS A " +
+                            $"WHERE NOT EXISTS (SELECT * FROM tbl_customer AS B WHERE B.CustomerCode = A.CACSAN OR B.CustomerName = A.CUNMFL OR TRY_CONVERT(decimal, B.CustomerCode) = A.CUCSN);")
                 .ToListAsync();
             return getMerchant;
         }
@@ -27,7 +29,8 @@ namespace CSI.Application.Services
         {
             var getMerchant = new List<Merchant>();
             getMerchant = await _dbContext.Merchant
-                .FromSqlRaw($"SELECT DISTINCT A.CACSAN[MerchantCode] , A.CUNMFL[MerchantName], CAST(A.CUCSN AS VARCHAR(500)) + ' P'[MerchantNo] FROM OPENQUERY([SNR], 'SELECT A.CUCSN, A.CUNMFL, B.CACSAN FROM MMJDALIB.CIMCUS A INNER JOIN MMJDALIB.CRMCSA B ON A.CUCSN = B.CACSN') AS A;")
+                .FromSqlRaw($"SELECT DISTINCT A.CACSAN[MerchantCode] , A.CUNMFL[MerchantName], CAST(A.CUCSN AS VARCHAR(500)) + ' P'[MerchantNo] " +
+                            $"FROM OPENQUERY([SNR], 'SELECT A.CUCSN, A.CUNMFL, B.CACSAN FROM MMJDALIB.CIMCUS A INNER JOIN MMJDALIB.CRMCSA B ON A.CUCSN = B.CACSN') AS A;")
                 .ToListAsync();
             return getMerchant;
         }
