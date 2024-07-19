e2fde032b64209580d9ec5f93707a733adcd6e85
fjuat <153488141+fjuat@users.noreply.github.com>
2024-07-19 15:09:29 +0800

    Merchant (#48)
    * origin/enhancement-240618
    
    - View Table
    - View Details
    - Resize Button on User Table
    
    * Maintenance/Merchant Enhancement
    
    Create/Update
    - Merchant Name & Merchant Number is now dropdown with search filter.
    - Merchant Code is autofill
    - Status is remove
    
    * Maintenance/Merchant Update
    
    Remove from dropdown
    -GrabMart
    -GrabFood
    -FoodPanda
    -PICK A ROO - FS
    -PICK A ROO - Merch
    -MetroMart
    -Lazada
    -Shopee
    -Employee
    -Bank Promos
    -GCash
    
    * 2024/07/19 - Maintenance/Merhant
    
    - Status Column is now Toggle (Active/InActive)
    
    * 2024/07/19 - Maintenance/Merhant
    
    2024/07/19 - Maintenance/Merhant
    Status is now a toggle button (Active/Inactive)
    
    ---------
    
    Co-authored-by: Richard Baluyut <133842568+chaaard@users.noreply.github.com>

--- CSI.Application/Services/MerchantService.cs
+++ CSI.Application/Services/MerchantService.cs
@@ -18,17 +18,19 @@ namespace CSI.Application.Services
         {
             var getMerchant = new List<Merchant>();
             getMerchant = await _dbContext.Merchant
-                .FromSqlRaw($"SELECT " +
-                $"CUCSN[MerchantCode], " +
-                $"CUNMFL[MerchantName], " +
-                $"CACSAN[MerchantNo] " +
-                $"FROM OPENQUERY([SNR],'SELECT " +
-                $"A.CUCSN, " +
-                $"A.CUNMFL, " +
-                $"B.CACSAN " +
-                $"FROM MMJDALIB.CIMCUS A " +
-                $"INNER JOIN MMJDALIB.CRMCSA B " +
-                $"ON A.CUCSN = B.CACSN');")
+                .FromSqlRaw($"SELECT DISTINCT A.CACSAN[MerchantCode] , A.CUNMFL[MerchantName], CAST(A.CUCSN AS VARCHAR(500)) + ' P'[MerchantNo] " +
+                            $"FROM OPENQUERY([SNR], 'SELECT A.CUCSN, A.CUNMFL, B.CACSAN FROM MMJDALIB.CIMCUS A INNER JOIN MMJDALIB.CRMCSA B ON A.CUCSN = B.CACSN') AS A " +
+                            $"WHERE NOT EXISTS (SELECT * FROM tbl_customer AS B WHERE B.CustomerCode = A.CACSAN OR B.CustomerName = A.CUNMFL OR TRY_CONVERT(decimal, B.CustomerCode) = A.CUCSN);")
+                .ToListAsync();
+            return getMerchant;
+        }
+
+        public async Task<List<Merchant>> GetAllMerchant()
+        {
+            var getMerchant = new List<Merchant>();
+            getMerchant = await _dbContext.Merchant
+                .FromSqlRaw($"SELECT DISTINCT A.CACSAN[MerchantCode] , A.CUNMFL[MerchantName], CAST(A.CUCSN AS VARCHAR(500)) + ' P'[MerchantNo] " +
+                            $"FROM OPENQUERY([SNR], 'SELECT A.CUCSN, A.CUNMFL, B.CACSAN FROM MMJDALIB.CIMCUS A INNER JOIN MMJDALIB.CRMCSA B ON A.CUCSN = B.CACSN') AS A;")
                 .ToListAsync();
             return getMerchant;
         }
--- CSI.Domain/Entities/Merchant.cs
+++ CSI.Domain/Entities/Merchant.cs
@@ -8,7 +8,7 @@ namespace CSI.Domain.Entities
 {
     public class Merchant
     {
-        public decimal? MerchantCode { get; set; }
+        public string? MerchantCode { get; set; } = string.Empty;
         public string? MerchantName { get; set; } = string.Empty;
         public string? MerchantNo { get; set; } = string.Empty;
     }
